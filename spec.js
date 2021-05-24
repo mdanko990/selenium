7cf51a2c16d67c90cc9c212444d7f7bb907d3db5
mdanko990 <62939304+mdanko990@users.noreply.github.com>
2021-05-24 13:42:20 +0300

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,18 +1,7 @@
 module.exports = {
-/*  init: () => {
-    nameInput = element(by.model('yourName'));
-    greeting = element(by.binding('yourName'));
-
-  },*/
   get: (string) => {
     return browser.get(string);
-  }/*,
-  setName: (name) => {
-    return nameInput.sendKeys(name);
   },
-  getGreetingText: () => {
-    return greeting.getText();
-  }*/
   getCurrentUrl: () => {
     return browser.getCurrentUrl();
   }
